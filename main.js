514cc34b19f18c404babca25d053ce85113febf7
homyhan <homyhan0616@gmail.com>
2023-07-01 19:41:42 +0700

    complete 1

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
-var domId = function (id) {
-    return document.getElementById(id);
-}
-domId("btnSignin").addEventListener('click', function () {
-    var bg_black = document.querySelector('.background_black');
-    var modalSignin = document.querySelector('.modal.signin');   
-    modalSignin.classList.add("visibleSignin");
-    bg_black.classList.add("visibleBg");     
-})
-domId('btnCloseSignin').addEventListener('click', function () {
-    var bg_black = document.querySelector('.background_black');
-    var modalSignin = document.querySelector('.modal.signin');   
-    modalSignin.classList.remove("visibleSignin");
-    bg_black.classList.remove("visibleBg");   
-})
 
-domId('btnSignup').addEventListener('click', function () {
-    var bg_black = document.querySelector('.background_black');
-    bg_black.classList.add("visibleBg"); 
-    var modalSignUp = document.querySelector('.modal.signup');   
-    modalSignUp.classList.add("visibleSignup");
-})
-    
-domId('btnCloseSignup').addEventListener('click', function () {
-    var bg_black = document.querySelector('.background_black');
-    var modalSignUp = document.querySelector('.modal.signup');   
-    modalSignUp.classList.remove("visibleSignup");
-    bg_black.classList.remove("visibleBg");   
-})
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
